e6bfa6f5a4f92d11cbeabbd9fc70d34d1fbc5de3
[polland] <[pollandsabisa@gmail.com]>
2022-05-08 14:43:16 +0300

    introduced another folder to make google_api known

--- php/handle_glogin.php
+++ php/handle_glogin.php
@@ -1,12 +1,12 @@
 <?php
 
-include "google_api/vendor/autoload.php";
+include "../googleapi/google_api/vendor/autoload.php";
 
 $gclient = new Google_Client();
 
 $gclient -> setClientId( "1069928971711-ed8ondbt41qlkffjf7j44mtneopofdnc.apps.googleusercontent.com" );
 $gclient -> setClientSecret( "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" );
-$gclient -> setApplicationName( "logistrack" );
+$gclient -> setApplicationName( "logitrack" );
 $gclient -> setRedirectUri( "http://localhost/logitrack/php/dashboard.php" );
 $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email"); 
 

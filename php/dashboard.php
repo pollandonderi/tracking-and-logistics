b1c4afa3b0ead143b44da06b73abac974f0e8007
[polland] <[pollandsabisa@gmail.com]>
2022-05-09 14:34:20 +0300

    g-login roblem. to be later worked on meanwhile doing dashboard

--- php/dashboard.php
+++ php/dashboard.php
@@ -1,18 +1,24 @@
 <?php
-
-// include "handle_glogin.php";
+session_start();
+ include "handle_glogin.php";
 
 
 // echo $_SESSION["secondname"];
 // echo $_SESSION["firstname"];
 // echo $_SESSION["email"];
 // // echo $_SESSION["gender"];
-// echo $_SESSION["picture"];
-
-
-
-
-
-
+// echo "<img src='".$_SESSION['picture']."'>";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>welcome</title>
+</head>
+<body>
+    
+</body>
+</html>
